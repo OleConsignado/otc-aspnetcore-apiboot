7eacc2a4e55cf68fd1e8305a6ac610c8f35b10cc
Ivan Borges <ivanborges@live.com>
2018-10-22 13:36:40 -0300

    Inclusão do projeto de teste. Será removido após validadas as novas configurações.

---
+++ Source/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Otc.AspNetCore.ApiBoot;
+
+namespace WebApplication1.Controllers
+{
+    [ApiVersion("1.0")]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Source/WebApplication1/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Otc.AspNetCore.ApiBoot;
+
+namespace WebApplication1
+{
+    public class Startup : ApiBootStartup
+    {
+        protected override ApiMetadata ApiMetadata => new ApiMetadata()
+        {
+            Name = "Olé Open API - Propostas",
+            Description = "API de acesso a recursos e operações relacionados a propostas",
+            DefaultApiVersion = "1.0"
+        };
+
+        public Startup(IConfiguration configuration)
+            : base(configuration)
+        {
+        }
+
+        protected override void ConfigureApiServices(IServiceCollection services)
+        {            
+        }
+    }
+}

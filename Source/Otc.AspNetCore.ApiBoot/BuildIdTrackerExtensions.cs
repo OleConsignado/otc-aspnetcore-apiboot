f03b8a7f8e40562c5e0c1e29034e94ff010d85eb
Matheus Neder <matheusneder@gmail.com>
2019-10-09 11:36:03 -0300

    Renamed BuildVersion to BuildId

---
+++ Source/Otc.AspNetCore.ApiBoot/BuildIdTrackerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Otc.AspNetCore.ApiBoot
+{
+    public static class BuildIdTrackerExtensions
+    {
+        public static IApplicationBuilder UseBuildIdTracker(this IApplicationBuilder app,
+            string version)
+        {
+            return app.UseMiddleware<BuildIdTrackerMiddleware>(version);
+        }
+    }
+}
